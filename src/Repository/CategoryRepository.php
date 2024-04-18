ef4eb79009bb1354e1bdd63fc25dc8310f7ad026
Jason Brastel <144421278+JasonBrastel@users.noreply.github.com>
2024-04-18 16:02:35 +0200

    Revert "bdd_jasonUser"

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository;
 
 use App\Entity\Category;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -20,12 +19,6 @@ class CategoryRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Category::class);
-    }
-
-    public function findAllOrderedByAscNameQueryBuilder(): QueryBuilder
-    {
-        return $this->createQueryBuilder('c')
-            ->orderBy('c.name', 'ASC');
     }
 
     //    /**
