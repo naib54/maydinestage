81636dfd09b87b7f53f1a6387abc83f7f5cd3918
tayfung88 <tayfungg88@gmail.com>
2024-05-02 15:36:44 +0200

    prblm résolu

---
+++ migrations/Version20240502102226.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240502102226 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        // $this->addSql('ALTER TABLE order_detail ADD stock_id INT DEFAULT NULL');
+        // $this->addSql('ALTER TABLE order_detail ADD CONSTRAINT FK_ED896F46DCD6110 FOREIGN KEY (stock_id) REFERENCES stock (id)');
+        // $this->addSql('CREATE INDEX IDX_ED896F46DCD6110 ON order_detail (stock_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        // $this->addSql('ALTER TABLE order_detail DROP FOREIGN KEY FK_ED896F46DCD6110');
+        // $this->addSql('DROP INDEX IDX_ED896F46DCD6110 ON order_detail');
+        // $this->addSql('ALTER TABLE order_detail DROP stock_id');
+    }
+}
